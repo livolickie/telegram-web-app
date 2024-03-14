0a94d66dfd05464eacb0db7b757fade1860deddb
DZwevo <havingteam@gmail.com>
2024-03-15 00:02:44 +0500

    ...

--- src/components/MainApp.tsx
+++ src/components/MainApp.tsx
@@ -8,40 +8,32 @@ const styles: Record<string, React.CSSProperties> = {
         textAlign: 'center',
     }
 }
-
-interface Product {
-    id: string,
-    name: string,
-    imageUrl: string,
-    price: number
-}
-
-// const products = [
-//     {
-//         id: 1,
-//         name: 'iPhone 11, белый 64GB',
-//         price: 40000,
-//         imageUrl: 'https://spb-apple.ru/image/cache/catalog/iphone%2011/White%201-700x700.jpg'
-//     },
-//     {
-//         id: 2,
-//         name: 'iPhone 12, фиолетовый 64GB',
-//         price: 50000,
-//         imageUrl: 'https://istudio-ufa.ru/a/istudio/files/multifile/2353/100032805910b0_1.webp'
-//     },
-//     {
-//         id: 3,
-//         name: 'iPhone 13 Pro Max, зеленый 128GB',
-//         price: 100000,
-//         imageUrl: 'https://spb-apple.ru/image/cache/catalog/Add/13%20pro%20max/a75487514ab2bd7b9018089b9c6016ef-700x700.jpg'
-//     },
-//     {
-//         id: 4,
-//         name: 'iPhone 14, желтый 128GB',
-//         price: 125000,
-//         imageUrl: 'https://store-apple.msk.ru/image/cache/catalog/tovary/iphone/iphone-14-plus/apple-iphone-14-14-plus-joltyy-800x800.jpg'
-//     }
-// ]
+const products = [
+    {
+        id: "09f1f2af-1377-4c6e-840b-bb6c663359ce",
+        name: 'iPhone 11, белый 64GB',
+        price: 40000,
+        imageUrl: 'https://spb-apple.ru/image/cache/catalog/iphone%2011/White%201-700x700.jpg'
+    },
+    {
+        id: "920cd6e1-c83b-4de5-ba13-726a2f1f37a5",
+        name: 'iPhone 12, фиолетовый 64GB',
+        price: 50000,
+        imageUrl: 'https://istudio-ufa.ru/a/istudio/files/multifile/2353/100032805910b0_1.webp'
+    },
+    {
+        id: "c76d0acb-3b7f-4866-a826-778abbd41cc5",
+        name: 'iPhone 13 Pro Max, зеленый 128GB',
+        price: 100000,
+        imageUrl: 'https://spb-apple.ru/image/cache/catalog/Add/13%20pro%20max/a75487514ab2bd7b9018089b9c6016ef-700x700.jpg'
+    },
+    {
+        id: "2b04f10d-ccff-423e-a470-cc5b607d7c7a",
+        name: 'iPhone 14, желтый 128GB',
+        price: 125000,
+        imageUrl: 'https://store-apple.msk.ru/image/cache/catalog/tovary/iphone/iphone-14-plus/apple-iphone-14-14-plus-joltyy-800x800.jpg'
+    }
+]
 
 interface SelectedItem {
     itemId: string,
@@ -50,25 +42,16 @@ interface SelectedItem {
 
 export function MainApp() {
     
-    const [products, setProducts] = useState<Product[]>([])
-    
     const {tg} = useTelegram()
 
     useEffect(() => {
         tg.ready();
-        
-        const urlParams = new URLSearchParams(window.location.search);
-
-        setProducts(JSON.parse(urlParams.get('products') ?? "[]") as Product[])
     }, []);
     
     const [selectedItems, setSelectedItems] = useState<SelectedItem[]>([])
 
     const onSendData = useCallback(() => {
-        const data = {
-            selectedItems
-        }
-        tg.sendData(JSON.stringify(data));
+        tg.sendData(JSON.stringify(selectedItems));
         setSelectedItems([])
     }, [selectedItems])
     
